91a080185080dcd65a86197b7d3121f5c02631db
jhoncc2 <jhoncc2@gmail.com>
2022-06-04 14:22:22 -0400

    add service that calls GITHUB commits API, then return the data from /commits endpoint

--- gitcommit-backend/src/commits/commits.controller.ts
+++ gitcommit-backend/src/commits/commits.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { CommitsService } from './commits.service';
-import { Observable, of } from 'rxjs';
-import { MockData } from './utils/mock';
+import { map, Observable } from 'rxjs';
 import { MockCommitsService } from './utils/mock.service';
 import { Commit } from './interface/commit.interface';
+import { AxiosResponse } from 'axios';
 
 /**
  * Class with endpoints for `/commits` path
@@ -21,6 +21,21 @@ export class CommitsController {
    */
   @Get()
   commits(): Observable<Commit[]> {
-    return this.mockService.getMockCommits()
+    return this.commitsService.getCommits().pipe(
+      map((res) => {
+        return res.data;
+      }),
+    );
   }
+
+  /**
+   * Returns Observable containing the `promise` that returns 
+   * the mock data collected from github
+   * 
+   * @returns Observable<Commit[]>
+   */
+   @Get('mock')
+   commitsMock(): Observable<Commit[]> {
+     return this.mockService.getMockCommits()
+   }
 }
--- gitcommit-backend/src/commits/commits.service.ts
+++ gitcommit-backend/src/commits/commits.service.ts
@@ -1,4 +1,8 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { Observable } from 'rxjs';
+import { Commit } from './interface/commit.interface';
 
 /**
  * This class contains methods that access REST API endpoints
@@ -6,4 +10,25 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class CommitsService {
 
+  static commitsUrl = 'https://api.github.com/repos/jhoncc2/github-hook/commits'
+
+  constructor(private httpService: HttpService) {}
+
+  /**
+   * Returns data of commits wrapped in an Observable.
+   *
+   * @returns Observable<Commit[]> Array of commits
+   */
+  getCommits(): Observable<AxiosResponse<Commit[]>> {
+    const res = this.httpService.get(
+      CommitsService.commitsUrl
+    );
+
+    // TODO: remove: log data
+    res.subscribe((a) => {
+      console.log(a.data[0]);
+    });
+
+    return res;
+  }
 }
