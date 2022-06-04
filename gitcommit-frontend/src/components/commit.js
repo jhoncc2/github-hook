a019920143c10b062915f6fa142d99f7ec2356bc
jhoncc2 <jhoncc2@gmail.com>
2022-06-04 18:28:54 -0400

    add link to repository

--- gitcommit-frontend/src/components/commit.js
+++ gitcommit-frontend/src/components/commit.js
@@ -3,14 +3,18 @@ import './commit.css'
 /**
  * Commit componet
  */
-const Commit = ({commit}) => {
+const Commit = ({commit, html_url}) => {
   return (
-    <div className='Commit'>
-      <div className='message'> {commit.message}</div>
-      <div className='info'> 
-        {/* <div className='name'>{commit.author.name}</div> */}
-        <div className='date'>{commit.author.date}</div>
-        <div className='email'>{commit.author.email}</div>
+    <div className='commitWrapper'>
+      <a className='commitLink' href={html_url} >
+        <img className='imageLink' src='/enlace.png' alt="image"/>
+      </a>
+      <div className='commit'>
+        <div className='message'> {commit.message}</div>
+        <div className='info'> 
+          <div className='date'>{commit.author.date}</div>
+          <div className='email'>{commit.author.email}</div>
+        </div>
       </div>
     </div>
   )
